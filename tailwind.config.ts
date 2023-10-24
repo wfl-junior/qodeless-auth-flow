65c455e55bd3ddc6603eb1bf1cb93dcd5289b83f
Wallace Júnior <wflj1997@gmail.com>
2023-10-24 19:45:20 -0300

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        app: {
+          blue: {
+            500: "#4461F2",
+            900: "#14162E",
+          },
+          yellow: {
+            500: "#DDA82A",
+          },
+          gray: {
+            100: "#EAF0F7",
+            200: "#DFDFDF",
+            300: "#C7C7C7",
+            500: "#4F555A",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
